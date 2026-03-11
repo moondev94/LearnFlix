e04ebc4c56e28a82dd23271748f8dce942a2ce57
Luan <luan.martins9406@gmail.com>
2026-03-11 00:50:13 -0300

    :recycle: refactor: mudança da tela de login da home

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,45 +1,37 @@
-import "../App.css";
-import { useState } from "react";
-import { Link } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser, faLock } from "@fortawesome/free-solid-svg-icons";
+import "../App.css"
+import { Link, Navigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { loginStudent } from "../store/authSlice";
 
 export default function Home() {
 
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.auth.user);
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    alert('Sucess!')
+  if (user?.role === "student") {
+    return <Navigate to="/dashboard/aluno" />;
   }
 
   return (
-    <div className="page-transition">
-      <div className="sign-page">
-        <form onSubmit={handleSubmit}>
-          <h2>Sign in</h2>
-          <div className="signup"> or <Link to="/signup">create an account</Link></div>
-          <div className="input-field">
-            <p>E-mail</p>
-            <input type="email" placeholder="E-mail" required onChange={(e) => setUsername(e.target.value)} /> {/**/}
-            <FontAwesomeIcon className="input-icon" icon={faUser} />
-          </div>
-          <div className="input-field">
-            <p>Password</p>
-            <input type="password" placeholder="Password" required onChange={(e) => setPassword(e.target.value)} />
-            <FontAwesomeIcon className="input-icon" icon={faLock} />
-          </div>
-          <div className="remember-login">
-            <label>
-              <input type="checkbox" name="" />
-              Remember me
-            </label>
-          </div>
-          <button>Sign in</button>
-        </form>
-      </div>
-    </div>
-  )
-}
+    <section className="page-transition">
 
+      <h2>Bem-vindo ao LearnFlix</h2>
+
+      <p>Organize sua vida acadêmica em um único lugar.</p>
+
+      <button
+        className="sign-in-btn"
+        onClick={() => dispatch(loginStudent())}
+      >
+        Entrar como aluno
+      </button>
+
+      <Link to="/signup">
+        <button className="sign-in-btn">
+          Criar uma conta
+        </button>
+      </Link>
+
+    </section>
+  );
+}
