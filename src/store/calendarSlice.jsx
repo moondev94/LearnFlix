67ce2de8b4fb24db79503cac2cf6556ed2cf025a
Luan <luan.martins9406@gmail.com>
2026-03-15 22:32:43 -0300

    :sparkles: feat(calendar): adicionar calendarSlice para gerenciar feriados acadêmicos

---
+++ src/store/calendarSlice.jsx
@@ -0,0 +1,53 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+
+// ===== API ===== //
+export const fetchHolidays = createAsyncThunk(
+  "calendar/fetchHolidays",
+  async () => {
+    const response = await fetch(
+      "https://brasilapi.com.br/api/feriados/v1/2026"
+    );
+
+    const data = await response.json();
+
+    return data;
+  }
+);
+
+const calendarSlice = createSlice({
+  name: "calendar",
+
+  initialState: {
+    holidays: [],
+    loading: false,
+    error: null,
+  },
+
+  reducers: {},
+
+  extraReducers: (builder) => {
+    builder
+
+      // carregando
+      .addCase(fetchHolidays.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+
+      // sucesso
+      .addCase(fetchHolidays.fulfilled, (state, action) => {
+        state.loading = false;
+        state.holidays = action.payload;
+      })
+
+      // erro
+      .addCase(fetchHolidays.rejected, (state) => {
+        state.loading = false;
+        state.error = "Erro ao carregar feriados";
+      });
+  },
+});
+
+
+export default calendarSlice.reducer;
