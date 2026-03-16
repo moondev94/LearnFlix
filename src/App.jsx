2df86fc5c7c64964f06cee761a0b6057966585c8
Luan <luan.martins9406@gmail.com>
2026-03-15 22:27:58 -0300

    :sparkles: feat(routes): adicionar rotas das novas páginas no App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import { Provider } from "react-redux";
+import { store } from "./store/store";
 import Header from './components/Header';
 import Home from './pages/home';
 import Signup from './pages/signup';
 import About from './pages/about'
+import StudentDashboard from "./pages/dashboards/StudentDashboard";
+import StudentProfile from "./pages/StudentProfile";
 
 
 
@@ -13,14 +17,18 @@ function App() {
 
   return (
     <>
+    <Provider store={store}>
       <Header />
       <main>
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/signup' element={<Signup />} />
           <Route path='/about' element={<About />} />
+          <Route path="/dashboard/aluno" element={<StudentDashboard />} />
+  <Route path="/perfil" element={<StudentProfile />} />
         </Routes>
       </main>
+      </Provider>
     </>
   )
 }
